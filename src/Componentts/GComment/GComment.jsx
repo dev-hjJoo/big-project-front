7a6f5b40665b31ee444396684385f0b86a959c0b
HyoJin Joo <hyozz@hanyang.ac.kr>
2024-07-03 16:17:31 +0900

    update GComment component
    - props 불러오는 방식(중괄호) 수정
    - AutoSize 적용 패키지 변경

--- src/Componentts/GComment/GComment.jsx
+++ src/Componentts/GComment/GComment.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import "./comment.scss"
 
-import { TextareaAutosize } from '@mui/base/TextareaAutosize';
+import TextareaAutosize from "react-textarea-autosize";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleArrowUp } from "@fortawesome/free-solid-svg-icons";
 
-const GComment = () => {
+const GComment = ({loadContent, addContentToLocalChatLog}) => {
     return (
         <div className="comment">
-            <TextareaAutosize placeholder='Message'/>
-            <FontAwesomeIcon icon={faCircleArrowUp} size='lg'/>
+            <TextareaAutosize placeholder='Message' onChange={loadContent} /> 
+            <div className="clickBtn" onClick={addContentToLocalChatLog}>
+                <FontAwesomeIcon icon={faCircleArrowUp} size='lg'/>
+            </div>
         </div>
     );
 };
