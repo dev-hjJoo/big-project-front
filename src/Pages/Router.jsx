a9ae6d29ee5675431c187335cc56bce76fd374fd
HyoJin Joo <hyozz@hanyang.ac.kr>
2024-07-12 13:46:54 +0900

    fix Router

--- src/Pages/Router.jsx
+++ src/Pages/Router.jsx
@@ -6,16 +6,9 @@ import JoinPresenter from './Login/JoinPresenter';
 import Chatbot from './Chatbot/ChatbotContainer';
 import FAQContainer from './FAQ/FAQContainer';
 import DBContainer from './Admin/AdminContainer';
-<<<<<<< HEAD
-import BoardDetail from './Commnunity/BoardDetail';
-import BoardList from './Commnunity/BoardList';
-import BoardWrite from './Commnunity/BoardWrite';
-import NewsDetail from './Home/NewsDetail';
-=======
 import OCRContainer from './OCR/OCRContainer';
 import BoardRouter from './Commnunity/BoardRouter';
 
->>>>>>> 70f1faa97a63ec44afa810f42ae9ef83910d229d
 
 const Router = () => {
 
@@ -28,15 +21,8 @@ const Router = () => {
             <Route path='/chat' element={<Chatbot/>} />
             <Route path='/faq' element={<FAQContainer />} />
             <Route path='/db' element={<DBContainer />} />
-<<<<<<< HEAD
-            <Route path="/community/list" element={<BoardList />} />
-            <Route path="/community/write" element={<BoardWrite />} />
-            <Route path="/community/detail/:id" element={<BoardDetail />} />
-            <Route path="/news/:id" element={<NewsDetail />} />
-=======
             <Route path="/community/*" element={<BoardRouter />} />
             <Route path="/ocr" element={<OCRContainer />} />
->>>>>>> 70f1faa97a63ec44afa810f42ae9ef83910d229d
         </Routes>
     );
 };
